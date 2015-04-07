88934bc46e878f5d3947d110514c346b0d729c65
Olivier Chafik <olivier.chafik@gmail.com>
2015-04-07 10:29:57 +0100

    Test SyntaxUtils.isNamedParam

--- src/test/scala/scalaxy/BaseTest.scala
+++ src/test/scala/scalaxy/BaseTest.scala
@@ -7,15 +7,15 @@ import org.scalatest.{ FlatSpecLike, Matchers }
 import scala.tools.reflect.ToolBox
 import scala.tools.reflect.FrontEnd
 
+import scala.reflect.runtime.currentMirror
+
 trait BaseTest extends FlatSpecLike with Matchers with MockFactory {
 
   val global = scala.reflect.runtime.universe
   import global._
 
-  import scala.reflect.runtime.currentMirror
-
   private[this] lazy val toolbox = currentMirror.mkToolBox()
 
-  def parse(s: String): Tree = 
+  def parse(s: String): Tree =
     toolbox.typecheck(toolbox.parse(s)).asInstanceOf[Tree]
 }
--- src/test/scala/scalaxy/SyntaxUtilsTest.scala
+++ src/test/scala/scalaxy/SyntaxUtilsTest.scala
@@ -12,7 +12,7 @@ class SyntaxUtilsTest extends BaseTest with SyntaxUtils {
     }
   }
 
-  it should "detect args that are specified of left to default" in {
+  it should "detect specified args (as opposed to default ones)" in {
     {
       val Block(List(_), f12) = parse("""
         def f(a: Int, b: Int) = ???
@@ -41,6 +41,34 @@ class SyntaxUtilsTest extends BaseTest with SyntaxUtils {
       val ArgsSpecified(true, true) = f12
       val ArgsSpecified(true, false) = f1
       val ArgsSpecified(false, false) = f0
+    }
+  }
+
+  object ArgsNamed {
+    def unapplySeq(tree: Apply) = Option(tree) collect {
+      case Apply(_, args) => args.map(isNamedParam)
+    }
+  }
+
+  it should "detect named params" in {
+    {
+      val Block(List(_, f12), fa12) = parse("""
+        def f(a: Int, b: Int) = ???
+        f(1, 2)
+        f(a = 1, 2)
+      """)
+      val ArgsNamed(Some(false), Some(false)) = f12
+      val ArgsNamed(Some(true), Some(false)) = fa12
+    }
+
+    {
+      val Block(List(_, _, f12), f1) = parse("""
+        def f(a: Int, b: Int = 0) = ???
+        f(1, b = 2)
+        f(a = 1)
+      """)
+      val ArgsNamed(Some(false), Some(true)) = f12
+      val ArgsNamed(Some(true), Some(false)) = f1
     }
   }
 }
